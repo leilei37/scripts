268661c2fe95fe9968a5ef81bbfd9d093240dfcd
leilei37 <93961282+leilei37@users.noreply.github.com>
2022-07-30 16:32:10 +0800

    TWAS

---
+++ osca moment.sh
@@ -0,0 +1,9 @@
+##osca
+./osca_Mac --efile ln.txt --gene-expression --make-bod --no-fid --out lnprofile
+#qc
+osca --befile myprofile --sd-min 0.02 --make-bod --out newprofile
+osca --befile myprofile --missing-ratio-probe 0.01 --make-bod --out newprofile
+./osca_Mac --befile lnprofile --pheno agri1.phen --linear --out my
+#mlm
+./osca_Mac --moa-exact --befile ln2profile --pheno agri1.phen --out mymlmexactagri1
+./osca_Mac --moa-exact --befile myprofile --pheno my.phen --task-num 1000 --task-id 1 --thread-num 10 --out my
